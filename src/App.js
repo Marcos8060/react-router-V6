7a2db36682af42e4e10dd212cc4fe2531ea36f63
Marcos8060 <marcosgav80@gmail.com>
2022-04-14 16:23:57 +0300

    created exact paths for all the pages

--- src/App.js
+++ src/App.js
@@ -5,12 +5,34 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <h3>React Router</h3>
-    </div>
+    <Router>
+       <div className="App">
+          <h3>React Router</h3>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About Us</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+       </div>
+       <Routes>
+          <Route exact path='/' element={< Home />}></Route>
+          <Route exact path='/about' element={< About />}></Route>
+          <Route exact path='/contact' element={< Contact />}></Route>
+       </Routes>
+    </Router>
   );
 }
 
